84b9942b2d8f9a91b8510ce1144bef2013eed10e
fareehaabbasi <afareeha355@gmail.com>
2025-02-27 21:10:22 +0500

    question no: 4

--- practice.js
+++ practice.js
@@ -1,13 +1,25 @@
 //Question no:1
 let result = 10 + 5 * 2;
 console.log(result);
+//Ans: 20
+
 
 //question no: 2
 let result2 = (20-5) / 3;
 console.log(result2);
+//Ans: 5
+
 
 //question no: 3
 let result3 = 15 % 4;
 console.log(result3);
+//Ans: 3
+
+
+//question no: 4
+let result4 = 8 + 2 * (10 / 2);
+console.log(result4);
+//Ans: 18
+
 
 
