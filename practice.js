87ef03a5283cd0fe95d61bf816971e6d763dc8a2
fareehaabbasi <afareeha355@gmail.com>
2025-03-06 21:58:03 +0500

    ALL QUESTION COMPLETE

--- practice.js
+++ practice.js
@@ -21,5 +21,49 @@ let result4 = 8 + 2 * (10 / 2);
 console.log(result4);
 //Ans: 18
 
+//question no: 5
+let result5 = 12 - 4 + 3 * 2;
+console.log(result5);
+//Ans: 14
+
+
+//Question no:6
+//Add 25 and 30, then multiply the sum by 2.
+let question = (25 + 30) * 2;
+console.log(question);
+//Ans: 110
+
+
+//Question no:7
+//Subtract 15 from 50, then divide the result by 5.
+let question1 = (15 - 50) / 5;
+console.log(question1);
+//Ans: -7
+
+
+//Question no:8
+//Find the remainder when 45 is divided by 7.
+let remainder = 45 % 7;
+console.log(remainder);
+//Ans: 3
+
+
+//Question no:9
+//Multiply 8 by 3, then add 10 to the result.
+let add = 8 * 3 + 10;
+console.log(add);
+//Ans: 34
+
+
+
+//Question no:10
+//are the number 6 using the exponentiation operator (**)
+let add1 = 6;
+add1 = add1**
+console.log(add1);
+//Ans: 6
+
+console.log((5 * 2) / 2 + 1);
+
 
 
