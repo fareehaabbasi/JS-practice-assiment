2c71047963f60e221cc90233eaa9ee5965842bfb
fareehaabbasi <afareeha355@gmail.com>
2025-03-08 02:25:15 +0500

    percentage calculator

--- practice.js
+++ practice.js
@@ -76,3 +76,27 @@
     
 // }
 
+
+let totalMark = +prompt("How much total marks?");
+let obtainedMarks = +prompt("How much obtained marks?");
+
+let percentage = (totalMark / obtainedMarks) * 100;
+
+if (percentage >= 90) {
+  console.log("Your Percentage is 90%. Congratulation, You Pass in First division");
+} else if (percentage >= 80) {
+  console.log("Your Percentage is 80%. Congratulation, You  pass in second division");
+} else if (percentage >= 70) {
+  console.log("Your Percentage is 70%. Congratulation, You  pass in second division");
+} else if (percentage >= 60) {
+  console.log("Your Percentage is 60%. Congratulation, You  pass in second division");
+} else if (percentage >= 50) {
+  console.log("Your Percentage is 50%. Congratulation, You  pass in second division");
+} else if (percentage >= 50) {
+  console.log("Your Percentage is 40%. Congratulation, You  pass in second division");
+} else if (percentage >= 35) {
+  console.log("Your Percentage is 35%. Congratulation, You  pass in third division");
+} else {
+  console.log("Sorry, You are fail. Please try again");
+}
+
