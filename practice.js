6973fac1103acde538099539d0cd52a379c7747f
fareehaabbasi <afareeha355@gmail.com>
2025-03-10 01:29:24 +0500

    complete

--- practice.js
+++ practice.js
@@ -1,83 +1,3 @@
-// //Question no:1
-// let result = 10 + 5 * 2;
-// console.log(result);
-// //Ans: 20
-
-
-// //question no: 2
-// let result2 = (20-5) / 3;
-// console.log(result2);
-// //Ans: 5
-
-
-// //question no: 3
-// let result3 = 15 % 4;
-// console.log(result3);
-// //Ans: 3
-
-
-// //question no: 4
-// let result4 = 8 + 2 * (10 / 2);
-// console.log(result4);
-// //Ans: 18
-
-// //question no: 5
-// let result5 = 12 - 4 + 3 * 2;
-// console.log(result5);
-// //Ans: 14
-
-
-// //Question no:6
-// //Add 25 and 30, then multiply the sum by 2.
-// let question = (25 + 30) * 2;
-// console.log(question);
-// //Ans: 110
-
-
-// //Question no:7
-// //Subtract 15 from 50, then divide the result by 5.
-// let question1 = (15 - 50) / 5;
-// console.log(question1);
-// //Ans: -7
-
-
-// //Question no:8
-// //Find the remainder when 45 is divided by 7.
-// let remainder = 45 % 7;
-// console.log(remainder);
-// //Ans: 3
-
-
-// //Question no:9
-// //Multiply 8 by 3, then add 10 to the result.
-// let add = 8 * 3 + 10;
-// console.log(add);
-// //Ans: 34
-
-
-
-// //Question no:10
-// //are the number 6 using the exponentiation operator (**)
-// let add1 = 6;
-// add1 = add1**
-// console.log(add1);
-// //Ans: 6
-
-// console.log((5 * 2) / 2 + 1);
-
-
-// let studentPresent = ("ali")
-// let Name = prompt("Name of the student");
-
-// if(Name === studentPresent) {
-//     console.log("this student is present");
-// }else{
-//     console.log("this students is absent");
-    
-// }
-
-
-
 // Percentage Calculator
 
 // let totalMark = +prompt("How much total marks?");
@@ -104,22 +24,33 @@
 // }
 
 
+
 // User Authentication
 
-let userName = "FareehaAbbasi"
-let email = "abbasi@email.com"
-let password = "abbasi123789"
-let number = +92-7895679
+// let userName = "FareehaAbbasi"
+// let email = "abbasi@email.com"
+// let password = "abbasi123789"
+// let number = +92-7895679
 
-let Name = prompt("Enter your name")
-let enNumber = prompt ("Enter your Number")
-let enEmail =  prompt("Enter your email")
-let enPassword = prompt ("Enter your password")
+// let Name = prompt("Enter your name")
+// let enNumber = prompt ("Enter your Number")
+// let enEmail =  prompt("Enter your email")
+// let enPassword = prompt ("Enter your password")
 
-if(Name === userName || enNumber === number || enEmail === email ||enPassword === password){
-    console.log("You're account has been Login");
-} else {
-    console.log("User Authentication is wrong");
+// if(Name === userName || enNumber === number || enEmail === email ||enPassword === password){
+//     console.log("You're account has been Login");
+// } else {
+//     console.log("User Authentication is wrong");
+// }
+
+
+
+//Number Checker
+
+let num = +prompt("Enter a number");
+
+if(num % 2 === 0) {
+    console.log("Even number");
+}else {
+    console.log("Odd number");
 }
-
-
